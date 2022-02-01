9163d3eaf38a7e34a21e26a602d0ff3cd42231c9
Célio <celiofagundes30@gmail.com>
2022-01-31 22:00:57 -0300

    cart count funcionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Header from './components/Header/Header.jsx'
 import SimpleSlider from './components/SlickCarousel/SlickCarousel.jsx'
 import styles from './main.module.css'
@@ -5,9 +6,24 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 import useWindowDimensions from './useWindowsDimensions'
 function App() {
   const { width } = useWindowDimensions()
+  const [productCount, setProductCount] = useState(0)
+  const [cartCount, setCartCount] = useState(0)
+
+  const productSelection= (action) =>{
+    if(action === 'minus' && productCount > 0){
+      setProductCount(productCount -1)
+    }if(action !== 'minus'){
+      setProductCount(productCount + 1)
+    }
+  }
+  const sendCount = () =>{
+    if(productCount > 0){ 
+      setCartCount(cartCount + productCount)
+    }
+  }
   return (
     <div className='App'>
-      <Header />
+      <Header cartCount={cartCount} removeFromCart = {() => setCartCount(0)}/>
       <div className={styles.wrapper}>
         <div className={styles.container}>
           <SimpleSlider width = {width}/>
@@ -26,15 +42,15 @@ function App() {
             </div>
             <div className={styles.buttonsBox}>
               <div>
-                <button>
+                <button  onClick={() => productSelection('minus')}>
                   <AiOutlineMinus size={16} />
                 </button>
-                <p>0</p>
-                <button>
+                <p>{productCount}</p>
+                <button onClick={() => productSelection('plus')}>
                   <AiOutlinePlus size={16} />
                 </button>
               </div>
-              <button className={styles.addToCart}>
+              <button className={styles.addToCart} onClick={sendCount}>
                 <svg width='21' height='20' xmlns='http://www.w3.org/2000/svg'>
                   <path
                     d='M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z'
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,9 @@ import styles from './styles.module.css'
 import useWindowDimensions from '../../useWindowsDimensions'
 
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
-const Header = () => {
-  const { width } = useWindowDimensions()
+
+const Header = ({cartCount,removeFromCart}) => {
+  const {width} = useWindowDimensions()
   const [menuOpen, setMenuOpen] = useState(false)
   const [cartOpen, setCartOpen] = useState(false)
 
@@ -21,7 +22,11 @@ const Header = () => {
     setCartOpen(!cartOpen)
     setMenuOpen(false)
   }
- 
+  useEffect(() =>{
+    if(width > 800){
+      setMenuOpen(false)
+    }
+  }),[width]
   return (
     <header>
       <div className={styles.container}>
@@ -58,7 +63,7 @@ const Header = () => {
         </div>
         <div className={styles.rightContainer}>
           <div className={styles.cartIcon} onClick={openCart}>
-            <div className={styles.cartCount}>1</div>
+            <div className={styles.cartCount}>{cartCount}</div>
             <svg width='22' height='20' xmlns='http://www.w3.org/2000/svg'>
               <path
                 d='M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z'
@@ -70,18 +75,22 @@ const Header = () => {
           {cartOpen && !menuOpen &&(
             <div className={styles.cartContainer}>
               <p className={styles.cartLabel}>Cart</p>
+              
               <section className={styles.cartInfo}>
+              {cartCount > 0 ?
+              <>
                 <div className={styles.productInfo}>
                   <img src={cartImg} className={styles.cartImg} alt='cart-product' />
                   <div className={styles.productDescription}>
                     <p>Fall Limited Edition Sneakers</p>
-                    <p>$125.00 x 3 $375.00</p>
+                    <p>$125.00 x {cartCount} <span>${(cartCount * 125.00).toFixed(2)}</span></p>
                   </div>
                   <svg
                     width='14'
                     height='16'
                     xmlns='http://www.w3.org/2000/svg'
                     xmlnsXlink='http://www.w3.org/1999/xlink'
+                    onClick={removeFromCart}
                   >
                     <defs>
                       <path
@@ -93,7 +102,11 @@ const Header = () => {
                   </svg>
                 </div>
                 <button className={styles.checkoutButton}>Checkout</button>
+                </>
+                :<p className={styles.emptyMessage}>Your cart is empty</p>
+              }
               </section>
+              
             </div>
           )}
           <img src={avatar} alt='user' className={styles.avatar} />
